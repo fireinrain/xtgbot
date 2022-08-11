ebdfc5de604abf279be8754c8a3f936598a81947
sunrise <575563079@qq.com>
2022-08-12 03:29:00 +0800

    update

--- src/main/kotlin/com/fireinrain/xtgbot/crawler/SearchJob.kt
+++ src/main/kotlin/com/fireinrain/xtgbot/crawler/SearchJob.kt
@@ -48,3 +48,5 @@ class SearchJob : AbstractJob() {
     }
 
 }
+
+
--- src/main/kotlin/com/fireinrain/xtgbot/crawler/SearchTypeEnum.kt
+++ src/main/kotlin/com/fireinrain/xtgbot/crawler/SearchTypeEnum.kt
@@ -11,7 +11,7 @@ import com.fireinrain.xtgbot.config.XtgBotConfig
 @Time    : 2022/8/10 3:51 AM
  */
 
-enum class SearchTypeEnum(index: Int, desc: String, apiTempUrl: String) {
+enum class SearchTypeEnum(val index: Int, val desc: String, val apiTempUrl: String) {
     // @@ will be replaced by query string
     CENSORED(1, "有码影片", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=1"),
     UNCENSORED(-1, "无码影片", XtgBotConfig.getConfig("JavbusBaseUrl") + "uncensored/search/@@&type=1"),
@@ -19,7 +19,15 @@ enum class SearchTypeEnum(index: Int, desc: String, apiTempUrl: String) {
     DIRECTOR(2, "导演", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=2"),
     PRODUCER(3, "制作商", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=3"),
     PUBLISER(4, "发行商", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=4"),
-    SERIES(5, "系列", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=5")
+    SERIES(5, "系列", XtgBotConfig.getConfig("JavbusBaseUrl") + "search/@@&type=5");
+
+
+    /**
+     * 获取查询的链接地址
+     */
+    fun getQueryString(query: String): String {
+        return this.apiTempUrl.replace("@@", query)
+    }
 
 
 }
