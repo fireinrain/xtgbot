819cc5998d5cc7bc973e06c1802cc26f6a6739c8
sunrise <575563079@qq.com>
2022-08-12 02:46:28 +0800

    update

--- src/main/kotlin/com/fireinrain/xtgbot/entity/FilmIntro.kt
+++ src/main/kotlin/com/fireinrain/xtgbot/entity/FilmIntro.kt
@@ -10,10 +10,18 @@ package com.fireinrain.xtgbot.entity
  */
 
 data class FilmIntro(
+    // 作品封面url
     var introPicUrl: String = "",
+    // 作品名称
     var filmName: String = "",
+    // 当前作品有资源情况下最高的分辨率
     var resolution: String = "",
+    // 番号
     var code: String = "",
+    // 发行日期
     var publishDate: String = "",
-    var filmDetailUrl: String = ""
+    // 作品详情页面url
+    var filmDetailUrl: String = "",
+    // 下一页 当没有下一页后 设置为 ""
+    var nextFetchPageUrl: String? = null
 ) : SearchResult
--- src/main/kotlin/com/fireinrain/xtgbot/entity/StarIntro.kt
+++ src/main/kotlin/com/fireinrain/xtgbot/entity/StarIntro.kt
@@ -10,8 +10,14 @@ package com.fireinrain.xtgbot.entity
  */
 
 data class StarIntro(
+    // 演员大头贴
     var imgPicUrl: String = "",
+    // 演员名字
     var starName: String = "",
+    // 是否为有码或是无码演员
     var isCensor: String = "",
-    var starPageUrl: String = ""
+    // 演员的个人作品主页
+    var starPageUrl: String = "",
+    // 下一个页面的url 当没有下一页后 设置为 ""
+    var nextFetchPageUrl: String? = null
 ) : SearchResult
