6357440f03bfa9b0b856b0a359d05ee4dc2edfaa
sunrise <575563079@qq.com>
2022-08-26 07:43:36 +0800

    update

---
+++ src/main/java/JavaMain.java
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @Author : fireinrain
+ * @Site : https://github.com/fireinrain
+ * @File : JavaMain
+ * @Software: IntelliJ IDEA
+ * @Time : 2022/8/26 7:23 AM
+ */
+
+public class JavaMain {
+    public static void main(String[] args) {
+        System.out.println("Java Main test");
+    }
+}
